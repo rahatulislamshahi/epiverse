b050b217010a1a469ff83047687773f323b9f72f
robertpattinson127477 <robertpattinson127477@gmail.com>
2025-12-30 17:10:16 +0000

    feat: clean up project structure and fix analyzer warnings

--- lib/profile/profile_screen.dart
+++ lib/profile/profile_screen.dart
@@ -6,7 +6,7 @@ class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
-  _ProfileScreenState createState() => _ProfileScreenState();
+  State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
